5ac8e2eb4284d8d94525e048ef50dbe160808706
tgibbons-css <tgibbons@css.edu>
2023-09-14 13:41:35 -0500

    Cleaned up some comments

--- app/src/main/java/css/cecprototype2/SensorCamera.java
+++ app/src/main/java/css/cecprototype2/SensorCamera.java
@@ -35,6 +35,10 @@ public class SensorCamera {
 
     // Constructor: initialize camera
     SensorCamera(Context context, PreviewView previewView) {
+        // should be passed the application context which is needed by the camera
+        // should also be passed the previewView on the screen where the image should be displayed
+        // TODO: is there a better way to connect the camera to the previewView?  Will the previewView change when the phone is rotated?
+
         startCameraProvider( context,  previewView);
     }
 
@@ -92,32 +96,40 @@ public class SensorCamera {
         //cameraProvider.bindToLifecycle(context, cameraSelector, imageAnalysis, imageCapture, imagePreview);
     }
 
+    public void capturePhoto() {
+        // public abstraction to take photo.
+        // Currently calls analyze photo, but could capture photo to save to disk later...
+        // The UI should call this through the MainViewModel
+        analyzePhotoProvider();
+    }
     private void analyzePhotoProvider() {
-        Log.i("TEG","Trying to Analyze Photo --- 111");
+        Log.i("CIS4444","Trying to Analyze Photo --- 111");
         executor = Executors.newSingleThreadExecutor();
-        Log.i("TEG","Trying to Analyze Photo --- 222");
+        Log.i("CIS4444","Trying to Analyze Photo --- 222");
         imageAnalysis.setAnalyzer(executor, new ImageAnalysis.Analyzer() {
             @OptIn(markerClass = ExperimentalGetImage.class) @Override
             public void analyze(@NonNull ImageProxy imageProxy) {
-                Log.i("TEG","Trying to Analyze Photo --- analyze callback 1");
+                Log.i("CIS4444","Trying to Analyze Photo --- analyze callback 1");
                 int rotationDegrees = imageProxy.getImageInfo().getRotationDegrees();
-                Log.i("TEG","analyze callback 1 --- rotationDegrees = "+rotationDegrees);
+                Log.i("CIS4444","analyze callback 1 --- rotationDegrees = "+rotationDegrees);
                 // Get the image proxy plane's buffer which is where the pixels are
                 ImageProxy.PlaneProxy[] planes = imageProxy.getPlanes();
-                Log.i("TEG","analyze callback 1 --- planes = "+planes.length);
+                Log.i("CIS4444","analyze callback 1 --- planes = "+planes.length);
 
                 ByteBuffer buffer = planes[0].getBuffer();
                 // Create a blank bitmap
-                Log.i("TEG","analyze callback 2");
+                Log.i("CIS4444","analyze callback 2");
                 bitmap = imageProxy.toBitmap();
                 image = imageProxy.getImage();
                 //bitmap = Bitmap.createBitmap(imageProxy.getWidth(),imageProxy.getHeight(),Bitmap.Config.ARGB_8888);
                 // copy the image proxy plane into the bitmap
                 //bitmap.copyPixelsFromBuffer(buffer);
-                Log.i("TEG", "analyze callback 2 --- bmp height = "+bitmap.getHeight());
+                Log.i("CIS4444", "analyze callback 2 --- bmp height = "+bitmap.getHeight());
 
-                // Crop!
+                // TODO: add code to crop to just the needed area of the photo
                 //bitmap = Bitmap.createBitmap(bitmap, rect.left, rect.top, rect.width(), rect.height())
+
+                // TODO: notify some software that the image has been updated
 
                 // after done, release the ImageProxy object
                 imageProxy.close();
