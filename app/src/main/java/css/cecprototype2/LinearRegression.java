3cf28a66acc2f3ccadde6fb219bf1830dab9d00b
Miles Liebren <mliebren@css.edu>
2023-10-26 12:10:04 -0500

    Merged Updated Linear Regression to Main

--- app/src/main/java/css/cecprototype2/LinearRegression.java
+++ app/src/main/java/css/cecprototype2/LinearRegression.java
@@ -1,21 +1,23 @@
 package css.cecprototype2;
 
-import java.util.Arrays;
-import java.util.List;
 import org.apache.commons.math3.stat.regression.SimpleRegression;
 
-public class LinearRegression {
-    // Data
-    List<Double> fluorescenceValues = Arrays.asList(1200.0, 1310.0, 1430.0, 1580.0);
-    List<Double> concentrationValues = Arrays.asList(0.2, 0.4, 0.6, 0.8);
-    // Create a SimpleRegression instance
-    SimpleRegression regression;
+import java.util.List;
 
-    public LinearRegression(List<Double> fluorescenceValues, List<Double> concentrationValues) {
-        this.fluorescenceValues = fluorescenceValues;
-        this.concentrationValues = concentrationValues;
+public class linearRegression {
+
+
+    public linearRegression(List<Double> xValues, List<Double> yValues) {
+        this.xValues = xValues;
+        this.yValues = yValues;
+        System.out.println("xValues size = "+xValues.size());
         leastSquaresAnalysis();
     }
+
+    SimpleRegression regression;
+    List <Double> xValues;
+    List <Double> yValues;
+
 
     public Double m_slope = 0.0;        // slope of linear regression line
     public Double b_intercept = 0.0;   // y-intercept
@@ -24,20 +26,17 @@ public class LinearRegression {
         // Create a SimpleRegression instance
         regression = new SimpleRegression();
         // Add data points to the regression model
-        for (int i = 0; i < fluorescenceValues.size(); i++) {
-            regression.addData(fluorescenceValues.get(i), concentrationValues.get(i));
+        for (int i = 0; i < xValues.size(); i++) {
+            regression.addData(xValues.get(i), yValues.get(i));
             //Log.d("CIS3334","Adding in x = "+xValues.get(i)+" and y = "+yValues.get(i));
-            System.out.println("Adding in x = "+ fluorescenceValues.get(i)+" and y = "+ concentrationValues.get(i));
+            System.out.println("Adding in x = "+xValues.get(i)+" and y = "+yValues.get(i));
         }
         // Perform linear regression
         m_slope = regression.getSlope();
         b_intercept = regression.getIntercept();
     }
-
-    // redict a value given a new sample using the linear regression model
-    public Double predict(Double newFluorescence) {
-        return regression.predict(newFluorescence);
+    public Double predict(Double newX) {
+        return regression.predict(newX);
     }
 
 }
-
