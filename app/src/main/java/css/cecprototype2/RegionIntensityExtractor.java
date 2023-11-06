ca9f7df718abbc7da9cface8a220fb76df7e1adc
Miles Liebren <mliebren@css.edu>
2023-11-06 12:48:06 -0600

    Fixes issue #2 and #3

--- app/src/main/java/css/cecprototype2/RegionIntensityExtractor.java
+++ app/src/main/java/css/cecprototype2/RegionIntensityExtractor.java
@@ -61,4 +61,41 @@ public class RegionIntensityExtractor
         }
     }
 
+    public Integer getAlterntiveRegionIntensity(Region inRegion) {
+        int sumIntensity = 0;
+        int numPixels = 0;
+        final int GREEN_THRESHOLD = 10; // Minimum green value threshold
+
+        // Get the Bitmap region for the given region
+        Bitmap regionBitmap = inRegion.getBitmapRegion(bitMap);
+
+        if (regionBitmap != null) {
+            int width = regionBitmap.getWidth();
+            int height = regionBitmap.getHeight();
+
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    int pixelColor = regionBitmap.getPixel(x, y);
+                    int green = Color.green(pixelColor);
+
+                    // Check if the green value is above the threshold
+                    if (green >= GREEN_THRESHOLD) {
+                        // Calculate pixel intensity using the green component
+                        sumIntensity += green;
+                        numPixels++;
+                    }
+                }
+            }
+        }
+
+        if (numPixels > 0) {
+            // Calculate and return the average intensity
+            int averageIntensity = sumIntensity / numPixels;
+            return averageIntensity;
+        } else {
+            // No pixels in the region that meet the criteria, return a default value
+            return 0;
+        }
+    }
+
 }
