9cff9ae866723aa08890c55a98b8dccee8309041
AustinOkechukwu <aokechukwu@css.edu>
2024-02-07 20:38:21 -0600

    Testing out Github

--- app/src/main/java/css/cecprototype2/main/MainActivity.java
+++ app/src/main/java/css/cecprototype2/main/MainActivity.java
@@ -55,6 +55,9 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+
+        //Test Update Austin
     }
 
 }
